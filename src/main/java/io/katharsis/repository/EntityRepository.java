ea5dffd7858c93118c3e2e00df5357883099fe1a
Patryk Orwat <patrykorwat@gmail.com>
2015-03-13 16:26:07 +0100

    base repository interface

--- src/main/java/io/katharsis/repository/EntityRepository.java
+++ src/main/java/io/katharsis/repository/EntityRepository.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  * @param <T> Type of an entity
  * @param <ID> Type of Identifier of an entity
  */
-public interface JsonRepository<T, ID extends Serializable> {
+public interface EntityRepository<T, ID extends Serializable> {
     
     <S extends T> S save(S entity);
     
@@ -16,8 +16,4 @@ public interface JsonRepository<T, ID extends Serializable> {
     Iterable<T> findAll(Iterable<ID> ids);
     
     void delete(ID id);
-    
-    <D> void addRelation(T source, D destination);
-    
-    <D> void removeRelation(T source, D destination);
 }
---
+++ src/main/java/io/katharsis/repository/RelationshipRepository.java
@@ -0,0 +1,23 @@
+package io.katharsis.repository;
+
+/**
+ * Base repository responsible for operations on relations 
+ * @param <T> source class type
+ * @param <D> destination class type
+ */
+public interface RelationshipRepository<T, D> {
+
+    /**
+     * Add relation from source to destination 
+     * @param source
+     * @param destination
+     */
+    void addRelation(T source, D destination);
+
+    /**
+     * Remove relation from source to destination
+     * @param source
+     * @param destination
+     */
+    void removeRelation(T source, D destination);
+}
