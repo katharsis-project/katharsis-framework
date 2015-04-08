378d2fbb2a49af7b787ac561eb13023d818891ab
Patryk Orwat <patrykorwat@gmail.com>
2015-04-09 00:25:00 +0200

    fixed repository

--- src/test/java/io/katharsis/rs/resource/repository/TaskToProjectRepository.java
+++ src/test/java/io/katharsis/rs/resource/repository/TaskToProjectRepository.java
@@ -6,35 +6,25 @@ import io.katharsis.rs.resource.model.Task;
 import org.jvnet.hk2.annotations.Service;
 
 @Service
-public class TaskToProjectRepository implements RelationshipRepository<Task, Long, Project, Long> {
+public class TaskToProjectRepository implements RelationshipRepository<Task, Long, Project> {
 
     @Override
-    public void addRelation(Task task, Project project) {
+    public void addRelation(Task task, Project project, String fieldName) {
 
     }
 
     @Override
-    public void removeRelation(Task task, Project project) {
+    public void removeRelation(Task task, Project project, String fieldName) {
 
     }
 
     @Override
-    public Project findOneTarget(Long aLong) {
+    public Project findOneTarget(Long sourceId, String fieldName) {
         return null;
     }
 
     @Override
-    public Iterable<Project> findTarget(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Long findOneTargetId(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Iterable<Long> findTargetIds(Long aLong) {
+    public Iterable<Project> findTargets(Long sourceId, String fieldName) {
         return null;
     }
 }
