f66b53f40a5deac78328bac9b23bd4044bfcaf97
Patryk Orwat <patrykorwat@gmail.com>
2015-04-09 22:19:50 +0200

    documentation

--- src/main/java/io/katharsis/resource/annotations/JsonApiToOne.java
+++ src/main/java/io/katharsis/resource/annotations/JsonApiToOne.java
@@ -3,7 +3,7 @@ package io.katharsis.resource.annotations;
 import java.lang.annotation.*;
 
 /**
- * Indicates an association to single value which need to be handled by a separate resource.
+ * Indicates an association to single value which need to be handled by a separate repository.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
