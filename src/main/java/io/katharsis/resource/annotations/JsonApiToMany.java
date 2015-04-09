144ce2e53470a7f86beb3d9b2fbc926d135dcf38
Patryk Orwat <patrykorwat@gmail.com>
2015-04-09 22:19:50 +0200

    documentation

--- src/main/java/io/katharsis/resource/annotations/JsonApiToMany.java
+++ src/main/java/io/katharsis/resource/annotations/JsonApiToMany.java
@@ -3,7 +3,7 @@ package io.katharsis.resource.annotations;
 import java.lang.annotation.*;
 
 /**
- * Indicates an association to many values which need to be handled by a separate resource.
+ * Indicates an association to many values which need to be handled by a separate repository.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
