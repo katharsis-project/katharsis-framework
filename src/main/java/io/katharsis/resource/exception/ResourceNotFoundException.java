f80cc3cb3062e3d5168828fde13db1f039ebabe6
Patryk Orwat <patrykorwat@gmail.com>
2015-04-16 22:33:20 +0200

    documentation update, closes #9

--- src/main/java/io/katharsis/resource/exception/ResourceNotFoundException.java
+++ src/main/java/io/katharsis/resource/exception/ResourceNotFoundException.java
@@ -2,6 +2,7 @@ package io.katharsis.resource.exception;
 
 /**
  * Thrown when resource for a type cannot be found.
+ * This exception should be handled by an exception mapper and corresponding 404 HTTP response should be generated.
  */
 public class ResourceNotFoundException extends ResourceException {
 
