dd64076449969856e1f6969bfdd07a0113c7a8f0
Błażej Krysiak <blazej.krysiak@gmail.com>
2015-05-02 20:34:46 +0200

    Added paging and sorting repositories.

---
+++ src/main/java/io/katharsis/repository/PageableAndSortableRepository.java
@@ -0,0 +1,15 @@
+package io.katharsis.repository;
+
+import io.katharsis.queryParams.SortingValues;
+
+import java.io.Serializable;
+import java.util.Map;
+
+public interface PageableAndSortableRepository<T, ID extends Serializable> extends PageableRepository<T, ID>,
+        SortableRepository<T, ID> {
+
+    Iterable<T> findPage(Map<String, SortingValues> sortingValuesMap);
+
+    Iterable<T> findPage(int offset, int limit, Map<String, SortingValues> sortingValuesMap);
+
+}
