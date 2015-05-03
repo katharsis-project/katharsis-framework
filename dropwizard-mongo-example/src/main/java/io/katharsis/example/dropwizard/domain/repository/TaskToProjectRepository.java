dd1643f6c1c7d0e13c458b4c2b075cf0885011f3
Patryk Orwat <patrykorwat@gmail.com>
2015-05-03 22:37:04 +0200

    workin' on relation repo

--- dropwizard-mongo-example/src/main/java/io/katharsis/example/dropwizard/domain/repository/TaskToProjectRepository.java
+++ dropwizard-mongo-example/src/main/java/io/katharsis/example/dropwizard/domain/repository/TaskToProjectRepository.java
@@ -3,22 +3,47 @@ package io.katharsis.example.dropwizard.domain.repository;
 import io.katharsis.example.dropwizard.domain.model.Project;
 import io.katharsis.example.dropwizard.domain.model.Task;
 import io.katharsis.repository.RelationshipRepository;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.bson.types.ObjectId;
 
-public class TaskToProjectRepository implements RelationshipRepository<Task, Long, Project, Long> {
+import javax.inject.Inject;
 
-    public void addRelation(Task task, Long projectId, String fieldName) {
+public class TaskToProjectRepository implements RelationshipRepository<Task, ObjectId, Project, ObjectId> {
+
+    private TaskRepository taskRepository;
+
+    @Inject
+    public TaskToProjectRepository(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public void addRelation(Task task, ObjectId objectId, String s) {
 
     }
 
-    public void removeRelation(Task task, Long projectId, String fieldName) {
+    @Override
+    public void removeRelation(Task task, ObjectId objectId, String s) {
 
     }
 
-    public Project findOneTarget(Long sourceId, String fieldName) {
-        return null;
+    @Override
+    public Project findOneTarget(ObjectId objectId, String s) {
+        Task task = taskRepository.findOne(objectId);
+        try {
+            return (Project) PropertyUtils.getProperty(task, s);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    public Iterable<Project> findTargets(Long sourceId, String fieldName) {
-        return null;
+    @Override
+    public Iterable<Project> findTargets(ObjectId objectId, String s) {
+        Task task = taskRepository.findOne(objectId);
+        try {
+            return (Iterable<Project>) PropertyUtils.getProperty(task, s);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
