6085059e1e04836179607cdab87f5fdd655a6319
Grzegorz Poznachowski <gpoznachowski@neoteric.eu>
2015-05-06 00:27:23 +0200

    Refactored ResourceInformationBuilder, covered duplicated @JsonApiId occurence.

--- src/main/java/io/katharsis/resource/ResourceInformation.java
+++ src/main/java/io/katharsis/resource/ResourceInformation.java
@@ -9,53 +9,44 @@ import java.util.Set;
  */
 public final class ResourceInformation {
 
-    private Class<?> resourceClass;
+    private final Class<?> resourceClass;
 
     /**
      * Found field of the id. Each resource has to contain a field marked by JsonApiId annotation.
      */
-    private Field idField;
+    private final Field idField;
 
     /**
      * A set of fields that has basic Java types (String, Long, ...).
      */
-    private Set<Field> basicFields;
+    private final Set<Field> basicFields;
 
     /**
      * A set of fields that contains non-standard Java types (List, Set, custom classes, ...).
      */
-    private Set<Field> relationshipFields;
+    private final Set<Field> relationshipFields;
+
+    public ResourceInformation(Class<?> resourceClass, Field idField, Set<Field> basicFields, Set<Field> relationshipFields) {
+        this.resourceClass = resourceClass;
+        this.idField = idField;
+        this.basicFields = basicFields;
+        this.relationshipFields = relationshipFields;
+    }
 
     public Class<?> getResourceClass() {
         return resourceClass;
-    }
-
-    public void setResourceClass(Class<?> resourceClass) {
-        this.resourceClass = resourceClass;
     }
 
     public Field getIdField() {
         return idField;
     }
 
-    public void setIdField(Field idField) {
-        this.idField = idField;
-    }
-
     public Set<Field> getBasicFields() {
         return basicFields;
     }
 
-    public void setBasicFields(Set<Field> basicFields) {
-        this.basicFields = basicFields;
-    }
-
     public Set<Field> getRelationshipFields() {
         return relationshipFields;
-    }
-
-    public void setRelationshipFields(Set<Field> relationshipFields) {
-        this.relationshipFields = relationshipFields;
     }
 
     public Field findRelationshipFieldByName(String name) {
--- src/main/java/io/katharsis/resource/ResourceInformationBuilder.java
+++ src/main/java/io/katharsis/resource/ResourceInformationBuilder.java
@@ -3,70 +3,67 @@ package io.katharsis.resource;
 import io.katharsis.resource.annotations.JsonApiId;
 import io.katharsis.resource.annotations.JsonApiToMany;
 import io.katharsis.resource.annotations.JsonApiToOne;
+import io.katharsis.resource.exception.ResourceException;
 import io.katharsis.resource.exception.ResourceFieldException;
 import io.katharsis.resource.exception.ResourceIdNotFoundException;
 
 import java.lang.reflect.Field;
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * A builder which creates ResourceInformation instances of a specific class. It extracts information about a resource
  * from annotations.
  */
-public class ResourceInformationBuilder {
+public final class ResourceInformationBuilder {
 
     public ResourceInformation build(Class<?> resourceClass) {
-
-        ResourceInformation resourceInformation = new ResourceInformation();
-        resourceInformation.setResourceClass(resourceClass);
         Field idField = getIdField(resourceClass);
-        resourceInformation.setIdField(idField);
-        resourceInformation.setBasicFields(getBasicFields(resourceClass, idField));
-        resourceInformation.setRelationshipFields(getRelationshipFields(resourceClass, idField));
-
-        return resourceInformation;
+        return new ResourceInformation(
+                resourceClass,
+                idField,
+                getBasicFields(resourceClass, idField),
+                getRelationshipFields(resourceClass, idField));
     }
 
     private <T> Field getIdField(Class<T> resourceClass) {
-        for (Field field : resourceClass.getDeclaredFields()) {
-            if (field.isAnnotationPresent(JsonApiId.class)) {
-                return field;
-            }
+        List<Field> idFields = Arrays.stream(resourceClass.getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(JsonApiId.class))
+                .collect(Collectors.toList());
+
+        if (idFields.size() == 0) {
+            throw new ResourceIdNotFoundException("Id field for class not found: " + resourceClass.getCanonicalName());
+        } else if (idFields.size() > 1) {
+            throw new ResourceException("Duplicated Id field found in class: " + resourceClass.getCanonicalName());
         }
-        throw new ResourceIdNotFoundException("Id field for class not found: " + resourceClass.getCanonicalName());
+        return idFields.get(0);
     }
 
     private <T> Set<Field> getBasicFields(Class<T> resourceClass, Field idField) {
-        Set<Field> fields = new HashSet<>();
-        for (Field field : resourceClass.getDeclaredFields()) {
-            if (!isRelationshipType(field) && !field.equals(idField) && !field.isSynthetic()) {
-                fields.add(field);
-                if (isRestrictedMember(field.getName())) {
-                    throw new ResourceFieldException("Field " + field.getName() + " of class "
-                            + resourceClass.getCanonicalName() + "is restricted");
-                }
-            }
-        }
-        return fields;
+        return Arrays.stream(resourceClass.getDeclaredFields())
+                .filter(field -> !isRelationshipType(field) && !field.equals(idField) && !field.isSynthetic()
+                        && verifyNotRestrictedMember(resourceClass, field.getName()))
+                .collect(Collectors.toSet());
     }
 
     private <T> Set<Field> getRelationshipFields(Class<T> resourceClass, Field idField) {
-        Set<Field> fields = new HashSet<>();
-        for (Field field : resourceClass.getDeclaredFields()) {
-            if (isRelationshipType(field) && !field.equals(idField)) {
-                if (isRestrictedMember(field.getName())) {
-                    throw new ResourceFieldException("Field " + field.getName() + " of class "
-                            + resourceClass.getCanonicalName() + "is restricted");
-                }
-                fields.add(field);
-            }
-        }
-        return fields;
+        return Arrays.stream(resourceClass.getDeclaredFields())
+                .filter(field -> isRelationshipType(field) && !field.equals(idField) && verifyNotRestrictedMember(resourceClass, field.getName()))
+                .collect(Collectors.toSet());
     }
 
     private boolean isRelationshipType(Field type) {
         return type.isAnnotationPresent(JsonApiToMany.class) || type.isAnnotationPresent(JsonApiToOne.class);
+    }
+
+    private <T> boolean verifyNotRestrictedMember(Class<T> resourceClass, String fieldName) {
+        if (isRestrictedMember(fieldName)) {
+            throw new ResourceFieldException("Field " + fieldName + " of class "
+                    + resourceClass.getCanonicalName() + "is restricted");
+        }
+        return true;
     }
 
     private boolean isRestrictedMember(String fieldName) {
