3524baf8391a9f610bb7b808caefb765e36158c5
Patryk Orwat <porwat@jitsolutions.pl>
2015-05-17 13:04:23 +0200

    documentation to annotations

--- src/main/java/io/katharsis/resource/annotations/JsonApiId.java
+++ src/main/java/io/katharsis/resource/annotations/JsonApiId.java
@@ -1,7 +1,16 @@
 package io.katharsis.resource.annotations;
 
+import io.katharsis.utils.parser.TypeParser;
+
 import java.lang.annotation.*;
 
+/**
+ * Defines a field which will be used as an identifier of a resource. It must be assigned to a field which implements
+ * {@link java.io.Serializable} and {@link TypeParser} is able to parse the value.
+ *
+ * @see <a href="http://jsonapi.org/format/#document-structure-resource-ids">JSON API - Resource IDs</a>
+ * @see TypeParser
+ */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
