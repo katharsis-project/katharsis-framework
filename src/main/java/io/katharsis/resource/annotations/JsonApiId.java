064677f5804fd04de82a9dad35ca0b34066d9e96
Patryk Orwat <patrykorwat@gmail.com>
2015-05-21 20:54:26 +0200

    Update JsonApiId.java

--- src/main/java/io/katharsis/resource/annotations/JsonApiId.java
+++ src/main/java/io/katharsis/resource/annotations/JsonApiId.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
  * Defines a field which will be used as an identifier of a resource. It must be assigned to a field which implements
  * {@link java.io.Serializable} and {@link TypeParser} is able to parse the value.
  *
- * @see <a href="http://jsonapi.org/format/#document-structure-resource-ids">JSON API - Resource IDs</a>
+ * @see <a href="http://jsonapi.org/format/#document-structure-resource-identification">JSON API - Resource Identification</a>
  * @see TypeParser
  */
 @Documented
