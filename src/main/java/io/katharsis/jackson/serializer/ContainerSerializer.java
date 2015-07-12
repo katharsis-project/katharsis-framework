c6ec292c1d6ad63aea26ca31d96209a61970b2e2
Grzegorz Poznachowski <gpoznachowski@neoteric.eu>
2015-07-13 00:23:55 +0200

    Exception constructor fix

--- src/main/java/io/katharsis/jackson/serializer/ContainerSerializer.java
+++ src/main/java/io/katharsis/jackson/serializer/ContainerSerializer.java
@@ -127,7 +127,7 @@ public class ContainerSerializer extends JsonSerializer<Container> {
         try {
             sourceId = BeanUtils.getProperty(data, idField.getName());
         } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
-            throw new JsonSerializationException("Exception while writing links", e);
+            throw new JsonSerializationException("Exception while writing links");
         }
         gen.writeStringField(SELF_FIELD_NAME, resourceUrl + "/" + sourceId);
     }
