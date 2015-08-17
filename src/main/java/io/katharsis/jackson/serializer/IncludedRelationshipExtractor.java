5107c562fe76e518203fe32f5fa858288f8e5ce3
Patryk Orwat <patrykorwat@gmail.com>
2015-08-17 22:30:23 +0200

    #69 slf4j

--- src/main/java/io/katharsis/jackson/serializer/IncludedRelationshipExtractor.java
+++ src/main/java/io/katharsis/jackson/serializer/IncludedRelationshipExtractor.java
@@ -7,6 +7,8 @@ import io.katharsis.resource.annotations.JsonApiIncludeByDefault;
 import io.katharsis.response.BaseResponse;
 import io.katharsis.response.Container;
 import io.katharsis.utils.PropertyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -16,6 +18,7 @@ import java.util.*;
  * Extracts inclusions from a resource.
  */
 public class IncludedRelationshipExtractor {
+    Logger logger = LoggerFactory.getLogger(IncludedRelationshipExtractor.class);
 
     public Set<?> extractIncludedResources(Object resource, Set<ResourceField> relationshipFields,
         BaseResponse response) {
@@ -24,7 +27,7 @@ public class IncludedRelationshipExtractor {
         try {
             includedResources.addAll(extractIncludedRelationships(resource, response));
         } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException | NoSuchFieldException e) {
-            // TODO log suppressed exception
+            logger.info("Exception while extracting included fields", e);
         }
 
         return includedResources;
