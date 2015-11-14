3f55b09d4a658d837255db6f506517e137b64010
Patryk Orwat <patrykorwat@gmail.com>
2015-11-14 20:46:01 +0100

    katharsis-spring-2 adjusted to katharsis-spring

--- spring-boot-simple-example/src/main/java/io/katharsis/example/springboot/simple/SpringBootSimpleExampleApplication.java
+++ spring-boot-simple-example/src/main/java/io/katharsis/example/springboot/simple/SpringBootSimpleExampleApplication.java
@@ -1,9 +1,12 @@
 package io.katharsis.example.springboot.simple;
 
+import io.katharsis.spring.boot.KatharsisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(KatharsisConfig.class)
 public class SpringBootSimpleExampleApplication {
 
     public static void main(String[] args) {
