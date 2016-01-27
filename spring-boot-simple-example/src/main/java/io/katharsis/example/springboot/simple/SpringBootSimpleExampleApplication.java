64fd5e037c93c8383055a3ba09b1d7edd7d7bc88
Patryk Orwat <patrykorwat@gmail.com>
2016-01-27 21:31:14 +0100

    v2.1.7

--- spring-boot-simple-example/src/main/java/io/katharsis/example/springboot/simple/SpringBootSimpleExampleApplication.java
+++ spring-boot-simple-example/src/main/java/io/katharsis/example/springboot/simple/SpringBootSimpleExampleApplication.java
@@ -1,13 +1,30 @@
 package io.katharsis.example.springboot.simple;
 
-import io.katharsis.spring.boot.KatharsisConfig;
+import io.katharsis.resource.registry.ResourceRegistry;
+import io.katharsis.spring.boot.KatharsisConfigV2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@Configuration
+@RestController
 @SpringBootApplication
-@Import(KatharsisConfig.class)
+@Import(KatharsisConfigV2.class)
 public class SpringBootSimpleExampleApplication {
+
+    @Autowired
+    private ResourceRegistry resourceRegistry;
+
+    @RequestMapping("/resourcesInfo")
+    public Map<?, ?> getResources(){
+        return resourceRegistry.getResources();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSimpleExampleApplication.class, args);
