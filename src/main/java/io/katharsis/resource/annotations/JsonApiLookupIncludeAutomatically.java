3b2cfe05376a568a52bdafbd6352531f6f5d18e1
Patryk Orwat <patrykorwat@gmail.com>
2016-03-22 20:20:44 +0100

    #243 javadoc for JsonApiLookupIncludeAutomatically

--- src/main/java/io/katharsis/resource/annotations/JsonApiLookupIncludeAutomatically.java
+++ src/main/java/io/katharsis/resource/annotations/JsonApiLookupIncludeAutomatically.java
@@ -1,9 +1,16 @@
 package io.katharsis.resource.annotations;
 
+import io.katharsis.queryParams.QueryParams;
+
+import java.io.Serializable;
 import java.lang.annotation.*;
 
 /**
- * Created by zachncst on 10/16/15.
+ * This annotation is used to make automatic value assignment using a defined relationship repository if such repository
+ * is available. It can be used to leave resource relationships from a resource repository not populated and make
+ * Katharsis call either {@link io.katharsis.repository.RelationshipRepository#findOneTarget(Serializable, String, QueryParams)}
+ * or {@link io.katharsis.repository.RelationshipRepository#findManyTargets(Serializable, String, QueryParams)}
+ * depending on the multiplicity of the relationship.
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
