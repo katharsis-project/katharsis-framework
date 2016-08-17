d21aed38bca747a106768c820c7b77ee1bff4de1
Patryk Orwat <patrykorwat@gmail.com>
2016-08-17 23:03:46 +0200

    #82 refactor

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -ev
 
 mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V -Dexamples=true
-mvn -pl '!:dropwizard-mongo-example,!:dropwizard-simple-example,!:jersey-example,!:spring-boot-simple-example,!:wildfly-example,!:katharsis-build,!:katharsis-examples,!:katharsis-parent' test jacoco:report coveralls:report
+mvn -pl ':katharsis-core' test jacoco:report coveralls:report
 if [ "${TRAVIS_PULL_REQUEST}" != "true" ] && [ "$(git status | head -1)" != "HEAD detached at FETCH_HEAD" ] ; then
 
     if [[ $TRAVIS_BRANCH == 'master' ]]; then
