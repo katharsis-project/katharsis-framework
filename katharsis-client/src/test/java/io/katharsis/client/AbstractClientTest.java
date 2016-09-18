4e973c0a41587422e89a95752208155d6ad7f84c
Craig Setera <craig@baseventure.com>
2016-09-18 09:31:37 -0500

    Adding testing support for validating request headers.

--- katharsis-client/src/test/java/io/katharsis/client/AbstractClientTest.java
+++ katharsis-client/src/test/java/io/katharsis/client/AbstractClientTest.java
@@ -1,11 +1,14 @@
 package io.katharsis.client;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Assert;
 import org.junit.Before;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,7 +26,7 @@ import io.katharsis.rs.KatharsisProperties;
 public abstract class AbstractClientTest extends JerseyTest {
 
 	protected KatharsisClient client;
-
+	protected TestApplication testApplication;
 	protected QueryParamsBuilder queryParamsBuilder = new QueryParamsBuilder(new DefaultQueryParamsParser());
 
 	@Before
@@ -40,35 +43,83 @@ public abstract class AbstractClientTest extends JerseyTest {
 
 	@Override
 	protected TestApplication configure() {
-		return new TestApplication(false);
+		if (testApplication == null) {
+			testApplication = new TestApplication(false);
+		}
+		
+		return testApplication;
 	}
 
 	@ApplicationPath("/")
 	public static class TestApplication extends ResourceConfig {
 
-		private KatharsisFeature feature;
+		private KatharsisTestFeature feature;
 
 		public TestApplication(boolean querySpec) {
 			property(KatharsisProperties.RESOURCE_SEARCH_PACKAGE, "io.katharsis.client.mock");
 			property(KatharsisProperties.RESOURCE_DEFAULT_DOMAIN, "http://test.local");
 
 			if (!querySpec) {
-				feature = new KatharsisFeature(new ObjectMapper(), new QueryParamsBuilder(new DefaultQueryParamsParser()),
-						new SampleJsonServiceLocator());
+				feature = new KatharsisTestFeature(
+					new ObjectMapper(), 
+					new QueryParamsBuilder(new DefaultQueryParamsParser()),
+					new SampleJsonServiceLocator());
 			}
 			else {
-				feature = new KatharsisFeature(new ObjectMapper(), new DefaultQuerySpecDeserializer(),
-						new SampleJsonServiceLocator());
+				feature = new KatharsisTestFeature(
+					new ObjectMapper(), 
+					new DefaultQuerySpecDeserializer(),
+					new SampleJsonServiceLocator());
 			}
 
 			feature.addModule(new TestModule());
 
 			register(feature);
-
 		}
 
 		public KatharsisFeature getFeature() {
 			return feature;
 		}
+	}
+
+	/**
+	 * Assert the specified header name has the specified value.
+	 * 
+	 * @param name
+	 * @param value
+	 */
+	protected void assertHasHeaderValue(String name, String value) {
+		MultivaluedMap<String, String> headers = getLastReceivedHeaders();
+		Assert.assertNotNull(headers);
+		
+		List<String> values = headers.get(name);
+		Assert.assertNotNull(values);
+		
+		Assert.assertTrue(values.contains(value));
+	}
+
+	/**
+	 * Clear the last received headers.
+	 */
+	protected void clearLastReceivedHeaders() {
+		getTestFilter().clearLastReceivedHeaders();
+	}
+
+	/**
+	 * Return the last received headers.
+	 * 
+	 * @return
+	 */
+	private MultivaluedMap<String, String> getLastReceivedHeaders() {
+		return getTestFilter().getLastReceivedHeaders();
+	}
+
+	/**
+	 * Return the configured test filter.
+	 * 
+	 * @return
+	 */
+	private TestRequestFilter getTestFilter() {
+		return ((KatharsisTestFeature) testApplication.getFeature()).getTestFilter();
 	}
 }
---
+++ katharsis-client/src/test/java/io/katharsis/client/KatharsisTestFeature.java
@@ -0,0 +1,61 @@
+package io.katharsis.client;
+
+import javax.ws.rs.core.FeatureContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.katharsis.locator.JsonServiceLocator;
+import io.katharsis.locator.SampleJsonServiceLocator;
+import io.katharsis.queryParams.QueryParamsBuilder;
+import io.katharsis.queryspec.DefaultQuerySpecDeserializer;
+import io.katharsis.rs.KatharsisFeature;
+
+/**
+ * Test-specific subclass of the {@link KatharsisFeature} used to 
+ * configure extra test-specific JAX-RS functionality.
+ * 
+ * @author Craig Setera
+ */
+public class KatharsisTestFeature extends KatharsisFeature {
+	private TestRequestFilter testRequestFilter;
+	
+	public KatharsisTestFeature(
+		ObjectMapper objectMapper, 
+		QueryParamsBuilder queryParamsBuilder,
+		JsonServiceLocator jsonServiceLocator) 
+	{
+		super(objectMapper, queryParamsBuilder, jsonServiceLocator);
+		testRequestFilter = new TestRequestFilter();
+	}
+
+	public KatharsisTestFeature(
+		ObjectMapper objectMapper, 
+		DefaultQuerySpecDeserializer defaultQuerySpecDeserializer,
+		SampleJsonServiceLocator jsonServiceLocator) 
+	{
+		super(objectMapper, defaultQuerySpecDeserializer, jsonServiceLocator);
+		testRequestFilter = new TestRequestFilter();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see io.katharsis.rs.KatharsisFeature#configure(javax.ws.rs.core.FeatureContext)
+	 */
+	@Override
+	public boolean configure(FeatureContext context) {
+		boolean result = super.configure(context);
+		context.register(testRequestFilter);
+		
+		return result;
+	}
+	
+	/**
+	 * Return the {@link TestRequestFilter} that is registered
+	 * with the feature.
+	 * 
+	 * @return
+	 */
+	public TestRequestFilter getTestFilter() {
+		return testRequestFilter;
+	}
+}
