ba3758eeababc0eb9cc61eb4cb3451622d76a1fe
Patryk Orwat <patrykorwat@gmail.com>
2016-09-27 22:31:04 +0200

    fixed compilation problem

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
+mvn clean install -DskipTests=true -Dmaven.javadoc.skip=true -B
 mvn -q -pl ':katharsis-core' test jacoco:report coveralls:report
 if [ "${TRAVIS_PULL_REQUEST}" != "true" ] && [ "$(git status | head -1)" != "HEAD detached at FETCH_HEAD" ] ; then
 
