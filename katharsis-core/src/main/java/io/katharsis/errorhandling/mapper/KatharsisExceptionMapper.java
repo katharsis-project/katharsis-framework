fec45dbc285fba86c7c65cf1a404d4a5356bf93c
Nick Mitchell <mitch2na@gmail.com>
2016-10-14 12:02:45 -0600

    #158 - Log error in KatharsisMappableException mapper

--- katharsis-core/src/main/java/io/katharsis/errorhandling/mapper/KatharsisExceptionMapper.java
+++ katharsis-core/src/main/java/io/katharsis/errorhandling/mapper/KatharsisExceptionMapper.java
@@ -2,14 +2,19 @@ package io.katharsis.errorhandling.mapper;
 
 import io.katharsis.errorhandling.ErrorResponse;
 import io.katharsis.errorhandling.exception.KatharsisMappableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Exception mapper for a generic exception which can be thrown in request processing.
  */
 public final class KatharsisExceptionMapper implements JsonApiExceptionMapper<KatharsisMappableException> {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public ErrorResponse toErrorResponse(KatharsisMappableException exception) {
+        logger.warn("failed to process request", exception);
         return ErrorResponse.builder()
                 .setStatus(exception.getHttpStatus())
                 .setSingleErrorData(exception.getErrorData())
