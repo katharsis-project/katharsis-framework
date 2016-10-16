bbd86030afa2b7d1f80b3ee9cc8681b028e60b15
PJ Weisberg <pj@milezero.com>
2016-10-15 17:40:49 -0700

    Fix after merge conflicts

--- katharsis-core/src/main/java/io/katharsis/queryspec/internal/QueryParamsAdapterBuilder.java
+++ katharsis-core/src/main/java/io/katharsis/queryspec/internal/QueryParamsAdapterBuilder.java
@@ -4,6 +4,8 @@ import java.util.Map;
 import java.util.Set;
 
 import io.katharsis.queryParams.QueryParamsBuilder;
+import io.katharsis.queryParams.context.SimpleQueryParamsParserContext;
+import io.katharsis.resource.information.ResourceInformation;
 import io.katharsis.resource.registry.ResourceRegistry;
 
 public class QueryParamsAdapterBuilder implements QueryAdapterBuilder {
@@ -19,6 +21,8 @@ public class QueryParamsAdapterBuilder implements QueryAdapterBuilder {
 
 	@Override
 	public QueryAdapter build(Class<?> resourceClass, Map<String, Set<String>> parameters) {
-		return new QueryParamsAdapter(resourceClass, queryParamsBuilder.buildQueryParams(parameters), resourceRegistry);
+		ResourceInformation info = resourceRegistry.getEntry(resourceClass).getResourceInformation();
+		SimpleQueryParamsParserContext context = new SimpleQueryParamsParserContext(parameters, info);
+		return new QueryParamsAdapter(resourceClass, queryParamsBuilder.buildQueryParams(context), resourceRegistry);
 	}
 }
