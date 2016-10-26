18d1a2b322e4893114e3b2a0c7fb79f1e7c601b8
remmeier <remo.meier@adnovum.ch>
2016-10-26 20:34:58 +0200

    documentation improvements

--- katharsis-core/src/main/java/io/katharsis/errorhandling/mapper/ExceptionMapper.java
+++ katharsis-core/src/main/java/io/katharsis/errorhandling/mapper/ExceptionMapper.java
@@ -2,20 +2,34 @@ package io.katharsis.errorhandling.mapper;
 
 import io.katharsis.errorhandling.ErrorResponse;
 
+/**
+ * ExceptionMapper supporting both Katharsis servers and clients.
+ */
 public interface ExceptionMapper<E extends Throwable> extends JsonApiExceptionMapper<E> {
 
+	/**
+	 * Converts the given exception to an JSON API response. Used on the server-side.
+	 */
 	@Override
-    ErrorResponse toErrorResponse(E exception);
+	ErrorResponse toErrorResponse(E exception);
 
 	/**
-	 * Convert the given error response to an exception.
+	 * Convert the given error response to an exception. Used on the client-side.
 	 *
 	 * @param errorResponse error response
-	 * @return true if mapper can handle the response
+	 * @return exception
 	 */
 	E fromErrorResponse(ErrorResponse errorResponse);
-	
+
+	/**
+	 * Decides whether the given errorResponse can be handled by this mapper. 
+	 * If true is returned, {@link #fromErrorResponse(ErrorResponse)} will be called.
+	 * 
+	 * If multiple mappers accept a given error response, the most specific exception is chosen, meaning
+	 * the one with the most superTypes.
+	 * 
+	 * @param errorResponse
+	 * @return true if it can be handled.
+	 */
 	boolean accepts(ErrorResponse errorResponse);
 }
-
-
