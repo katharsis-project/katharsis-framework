5acb7b39b56d40de7392bb343321cf5f1724e21e
Remo <remo.meier@adnovum.ch>
2016-11-14 11:21:23 +0100

    fixes

--- katharsis-rs/src/main/java/io/katharsis/rs/KatharsisFilter.java
+++ katharsis-rs/src/main/java/io/katharsis/rs/KatharsisFilter.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
 
