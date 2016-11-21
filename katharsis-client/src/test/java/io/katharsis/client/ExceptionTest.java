7af706260771f7195fba69dfdad5026870f250c1
Remo <remo.meier.ch@gmail.com>
2016-11-21 18:10:45 +0100

    #221 Support lazy proxies for katharsis-client (#231)
    fixed invocationTargetException vs. application specific exception when going trough a proxy. Consumer no longer receives a invocationTargetException. Instead he gets the proper application
    exception.

--- katharsis-client/src/test/java/io/katharsis/client/ExceptionTest.java
+++ katharsis-client/src/test/java/io/katharsis/client/ExceptionTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import io.katharsis.client.mock.models.Task;
 import io.katharsis.client.module.TestException;
+import io.katharsis.client.mock.models.Schedule;
+import io.katharsis.client.mock.repository.ScheduleRepository;
 
 public class ExceptionTest extends AbstractClientTest {
 
@@ -18,12 +20,28 @@ public class ExceptionTest extends AbstractClientTest {
 	}
 
 	@Test
-	public void test() {
+	public void genericRepo() {
 		Task task = new Task();
 		task.setId(10000L);
 		task.setName("test");
 		try {
 			taskRepo.create(task);
+			Assert.fail();
+		}
+		catch (TestException e) {
+			Assert.assertEquals("msg", e.getMessage());
+		}
+	}
+	
+	@Test
+	public void repoWithProxyAndInterface() {
+		ScheduleRepository repo = client.getResourceRepository(ScheduleRepository.class);
+		
+		Schedule schedule = new Schedule();
+		schedule.setId(10000L);
+		schedule.setName("test");
+		try {
+			repo.create(schedule);
 			Assert.fail();
 		}
 		catch (TestException e) {
