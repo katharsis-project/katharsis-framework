d9c047ed595e582da772550a561baf4583f546a4
remmeier <remo.meier@adnovum.ch>
2016-12-03 11:08:00 +0100

    more test coverage

--- katharsis-core/src/test/java/io/katharsis/utils/PreconditionUtilTest.java
+++ katharsis-core/src/test/java/io/katharsis/utils/PreconditionUtilTest.java
@@ -1,16 +1,36 @@
 package io.katharsis.utils;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PreconditionUtilTest {
 
 	@Test
+	public void testConstructorIsPrivate()
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+		Constructor<PreconditionUtil> constructor = PreconditionUtil.class.getDeclaredConstructor();
+		Assert.assertTrue(Modifier.isPrivate(constructor.getModifiers()));
+		constructor.setAccessible(true);
+		constructor.newInstance();
+	}
+
+	@Test
 	public void testSatisfied() {
+		PreconditionUtil.assertEquals(null, null, null);
 		PreconditionUtil.assertEquals(null, 1, 1);
 		PreconditionUtil.assertTrue(null, true);
 		PreconditionUtil.assertFalse(null, false);
 		PreconditionUtil.assertNotNull(null, "test");
 		PreconditionUtil.assertNull(null, null);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testObjectEqualsNotSatisfied() {
+		PreconditionUtil.assertEquals("message", new Object(), new Object());
 	}
 
 	@Test(expected = IllegalStateException.class)
