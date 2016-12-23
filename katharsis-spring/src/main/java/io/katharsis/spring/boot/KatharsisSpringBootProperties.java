11bf32c90ee8c4930ff60520f304c757d20d9854
Casey Link <ctlink@binaryelysium.com>
2016-12-23 02:04:01 +0100

    #264 Properties to configure default katharsis paging behavior (#265)
    * * adds max-page-limit property
    * cleans up documentation for spring boot properties configuration
    
    * Increase code coverage

--- katharsis-spring/src/main/java/io/katharsis/spring/boot/KatharsisSpringBootProperties.java
+++ katharsis-spring/src/main/java/io/katharsis/spring/boot/KatharsisSpringBootProperties.java
@@ -5,9 +5,31 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("katharsis")
 public class KatharsisSpringBootProperties {
     private String resourcePackage;
+
+    /**
+     * The FQDN of the running server. It is used when building link objects in responses. The value must not end
+     * with /.
+     */
     private String domainName;
+
+    /**
+     * Default prefix of a URL path used in two cases:
+     * <ul>
+     * <li>When building links objects in responses</li>
+     * <li>When performing method matching</li>
+     * </ul>
+     */
     private String pathPrefix;
-	private Long defaultPageLimit;
+
+    /**
+     * Default limit of pages.
+     */
+    private Long defaultPageLimit;
+
+    /**
+     * Maximum size of pages.
+     */
+    private Long maxPageLimit;
 
     public String getResourcePackage() {
         return resourcePackage;
@@ -40,4 +62,12 @@ public class KatharsisSpringBootProperties {
 	public void setDefaultPageLimit(Long defaultPageLimit) {
 		this.defaultPageLimit = defaultPageLimit;
 	}
+
+    public Long getMaxPageLimit() {
+        return maxPageLimit;
+    }
+
+    public void setMaxPageLimit(Long maxPageLimit) {
+        this.maxPageLimit = maxPageLimit;
+    }
 }
