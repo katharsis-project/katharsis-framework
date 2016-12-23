36614e2737b1946face42e7a1900ca8ccf6befe5
Błażej Krysiak <blazej.krysiak@gmail.com>
2016-12-23 01:16:20 +0100

    Respond with 404 status for single resources that do not exist - #272

--- katharsis-core/src/main/java/io/katharsis/dispatcher/controller/resource/ResourceGet.java
+++ katharsis-core/src/main/java/io/katharsis/dispatcher/controller/resource/ResourceGet.java
@@ -1,8 +1,9 @@
 package io.katharsis.dispatcher.controller.resource;
 
-import java.io.Serializable;
-
 import io.katharsis.dispatcher.controller.HttpMethod;
+import io.katharsis.errorhandling.ErrorData;
+import io.katharsis.errorhandling.ErrorDataBuilder;
+import io.katharsis.errorhandling.ErrorResponse;
 import io.katharsis.queryspec.internal.QueryAdapter;
 import io.katharsis.repository.RepositoryMethodParameterProvider;
 import io.katharsis.request.dto.RequestBody;
@@ -15,9 +16,12 @@ import io.katharsis.resource.registry.RegistryEntry;
 import io.katharsis.resource.registry.ResourceRegistry;
 import io.katharsis.resource.registry.repository.adapter.ResourceRepositoryAdapter;
 import io.katharsis.response.BaseResponseContext;
+import io.katharsis.response.HttpStatus;
 import io.katharsis.response.JsonApiResponse;
 import io.katharsis.response.ResourceResponseContext;
 import io.katharsis.utils.parser.TypeParser;
+
+import java.io.Serializable;
 
 public class ResourceGet extends ResourceIncludeField {
 
@@ -27,7 +31,7 @@ public class ResourceGet extends ResourceIncludeField {
 
     /**
      * {@inheritDoc}
-     *
+     * <p>
      * Checks if requested resource method is acceptable - is a GET request for a resource.
      */
     @Override
@@ -39,30 +43,47 @@ public class ResourceGet extends ResourceIncludeField {
 
     /**
      * {@inheritDoc}
-     *
+     * <p>
      * Passes the request to controller method.
      */
     @Override
     public BaseResponseContext handle(JsonPath jsonPath, QueryAdapter queryAdapter, RepositoryMethodParameterProvider
-        parameterProvider, RequestBody requestBody) {
+            parameterProvider, RequestBody requestBody) {
+
         String resourceName = jsonPath.getElementName();
-        PathIds resourceIds = jsonPath.getIds();
         RegistryEntry registryEntry = resourceRegistry.getEntry(resourceName);
+
+        PathIds resourceIds = jsonPath.getIds();
+        String id = resourceIds.getIds().get(0);
+
         if (registryEntry == null) {
             throw new ResourceNotFoundException(resourceName);
         }
-        String id = resourceIds.getIds().get(0);
 
-        @SuppressWarnings("unchecked") Class<? extends Serializable> idClass = (Class<? extends Serializable>) registryEntry
-                .getResourceInformation()
-                .getIdField()
-                .getType();
-        Serializable castedId = typeParser.parse(id, idClass);
-        ResourceRepositoryAdapter resourceRepository = registryEntry.getResourceRepository(parameterProvider);
-        @SuppressWarnings("unchecked")
-        JsonApiResponse response = resourceRepository.findOne(castedId, queryAdapter);
-        includeFieldSetter.setIncludedElements(resourceName, response, queryAdapter, parameterProvider);
+        try {
+            @SuppressWarnings("unchecked") Class<? extends Serializable> idClass = (Class<? extends Serializable>) registryEntry
+                    .getResourceInformation()
+                    .getIdField()
+                    .getType();
+            Serializable castedId = typeParser.parse(id, idClass);
+            ResourceRepositoryAdapter resourceRepository = registryEntry.getResourceRepository(parameterProvider);
 
-        return new ResourceResponseContext(response, jsonPath, queryAdapter);
+            @SuppressWarnings("unchecked")
+            JsonApiResponse response = resourceRepository.findOne(castedId, queryAdapter);
+            includeFieldSetter.setIncludedElements(resourceName, response, queryAdapter, parameterProvider);
+            return new ResourceResponseContext(response, jsonPath, queryAdapter);
+
+        } catch (ResourceNotFoundException e) {
+            ErrorData error = ErrorData.builder()
+                    .setStatus("404")
+                    .setTitle("Not Found")
+                    .setDetail("Could not find a resource of type '" + resourceName + "' with id '" + id + "'.")
+                    .build();
+
+            return ErrorResponse.builder()
+                    .setStatus(HttpStatus.NOT_FOUND_404)
+                    .setSingleErrorData(error)
+                    .build();
+        }
     }
 }
