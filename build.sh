08e2936b4d32a7a60fef9c21c917167ab17eaa8a
remmeier <remo.meier@adnovum.ch>
2017-03-02 21:08:38 +0100

    API browser #165
    travis setup

--- build.sh
+++ build.sh
@@ -2,21 +2,15 @@
 set -ev
 
 
-cd katharsis-ui
-npm install
-ng build
-cd..
-
-
-mvn clean install -P ui jacoco:report coveralls:report -Dmaven.javadoc.skip=true -B
+mvn clean install -Pui jacoco:report coveralls:report -Dmaven.javadoc.skip=true -B
 if [ "${TRAVIS_PULL_REQUEST}" != "true" ] && [ "$(git status | head -1)" != "HEAD detached at FETCH_HEAD" ] ; then
 
     if [[ $TRAVIS_BRANCH == 'master' ]]; then
       # deploy to staging repository
       echo "TODO: ADD DEPLOYMENT TO STAGING REPOSITORY"
     elif [[ $TRAVIS_BRANCH == 'develop' ]]; then
-      mvn clean deploy -DskipTests=true --settings settings.xml
+      mvn clean deploy -Pui -DskipTests=true --settings settings.xml
     else
-      mvn clean install -DskipTests=true --settings settings.xml
+      mvn clean install -Pui -DskipTests=true --settings settings.xml
     fi
 fi
