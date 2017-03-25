00c2173c4f36745818f4c4af604935493a5e8573
remmeier <remo.meier@adnovum.ch>
2017-03-25 18:50:20 +0100

    katharsis-jpa: support non-JPA supertypes for entities #384
    ignored non-jpa super types while computing meta/information objects.

---
+++ katharsis-jpa/src/test/java/io/katharsis/jpa/model/NonJpaBaseType.java
@@ -0,0 +1,14 @@
+package io.katharsis.jpa.model;
+
+public class NonJpaBaseType {
+
+	private String nonJpaValue;
+
+	public String getNonJpaValue() {
+		return nonJpaValue;
+	}
+
+	public void setNonJpaValue(String nonJpaValue) {
+		this.nonJpaValue = nonJpaValue;
+	}
+}
