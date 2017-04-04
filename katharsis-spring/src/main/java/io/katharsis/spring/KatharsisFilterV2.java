183a592a662861413b677697bcc5b0afbeac2743
Yuval Bonen <yuval@broadsay.com>
2017-04-04 10:07:20 +0300

    Fix for bug# 325

--- katharsis-spring/src/main/java/io/katharsis/spring/KatharsisFilterV2.java
+++ katharsis-spring/src/main/java/io/katharsis/spring/KatharsisFilterV2.java
@@ -82,7 +82,7 @@ public class KatharsisFilterV2 implements Filter, BeanFactoryAware {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+        if (req instanceof HttpServletRequest && res instanceof HttpServletResponse && isAcceptablePath((HttpServletRequest)req)) {
             HttpServletRequest request = new BufferedRequestWrapper((HttpServletRequest) req);
             HttpServletResponse response = (HttpServletResponse) res;
             req.setCharacterEncoding("UTF-8");
